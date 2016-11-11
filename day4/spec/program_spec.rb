686cb2ffe315dca5887e8043bedd8a6b3d6320de
dniman <dade.niman@gmail.com>
2016-11-11 12:38:37 +0300

    refactoring

--- day4/spec/program_spec.rb
+++ day4/spec/program_spec.rb
@@ -1,13 +1,13 @@
 module Day4
   RSpec.describe Program do
     describe "#start" do
+      let(:message){ capture(:stdout){ Day4::Program.new.start("abcdef") } }
+
       it "sends answer message" do
-        message = capture(:stdout){ Day4::Program.new.start("abcdef") }
         expect(message).to match(/Answer: /)
       end
 
       it "sends number in decimal in answer message" do
-        message = capture(:stdout) { Day4::Program.new.start("abcdef") }
         expect(message).to match(/\d+/)       
       end
     end
